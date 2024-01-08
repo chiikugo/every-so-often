c209fd58e6749b915e4168dc402ff1b072e9c334
chiku <chikunwenyi@gmail.com>
2024-01-08 12:50:43 -0500

    adding to journal, gonna merge

--- backend/index.ts
+++ backend/index.ts
@@ -12,16 +12,44 @@ app.use(cors())
 
 app.get("/api/notes", async (req, res) => {
     
-    const notes = await prisma.not.findMany();
+    const notes = await prisma.note.findMany();
     
     res.json(notes);
+    
 
 
 })
 
 
-app.listen(5000, ()=> {
 
-    console.log("server running on localhost:5000")
+app.post("/api/notes", async(req, res) => {
+    const {title, content} = req.body;
+
+    if (!title || !content) {
+        return res
+        .status(400)
+        .send("title and content required");
+    }
+    
+    try {
+        const note = await prisma.note.create({
+            data: {title, content}
+        })
+        res.json(note);
+        
+    } catch (error) {
+        res.
+        status(500)
+        .send("error please backtrack");
+        
+    }
+
+
+    
+})
+
+app.listen(5003, ()=> {
+
+    console.log("server running on localhost:5002")
 
 })
